40de459063757dd21d35c5d606e689637ad89b9c
Viorry <96753898+Viorry@users.noreply.github.com>
2023-11-20 10:00:56 +0800

    Add files via upload

---
+++ php/kategori.php
@@ -0,0 +1,34 @@
+<div class="row justify-content-between">
+    <div class="col-10"><b>Kategori</b></div>
+    <div class="col-2 d-flex justify-content-end align-items-end">
+        <button btn btn-dark btn-sm>add</button>
+    </div>
+</div>
+<hr>
+<table class="table table-sm border border-dark">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col" width="5%">No</th>
+            <th scope="col">Nama</th>
+            <th scope="col" width="15%">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+        $stmt = "SELECT * FROM kategori 
+        ";
+        $datas = mysqli_query($conn, $stmt);
+        while ($data = mysqli_fetch_array($datas)) {
+        ?>
+        
+            <tr>
+                <th scope="row"><?= $data['id_kategori'] ?></th>
+                <td><?= $data['nama_kategori'] ?></td>
+                <td>
+                    <button class="btn btn-dark btn-sm">Edit</button>
+                    <button class="btn btn-dark btn-sm">Delete</button>
+                </td>
+            </tr>
+        <?php } ?>
+  </tbody>
+</table>
---
+++ php/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020012_i putu abdiparta";
+
+    $conn = mysqli_connect($server, $user, $password, $database);
+
+?>
